5f8d920771077e381c6bc58d6977b416eaac1ea4
YuehuiYuan <31993274+YuehuiYuan@users.noreply.github.com>
2020-02-01 11:00:06 +0800

    init code

---
+++ jhgw-x-shop/miniprogram/pages/index/index.js
@@ -0,0 +1,62 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+
+    // 首页：swiper 功能变量
+    imgUrls: [
+      '/images/the-girl-1.jpg',
+      '/images/the-girl-2.jpg',
+      '/images/the-bookstore.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+
+    // 下面是demo程序原来变量
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+})
